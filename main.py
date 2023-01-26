d8e3bde2029d4ee7104afcc2fcd3daa1d4346892
rcdtx <29317800+rcdtx@users.noreply.github.com>
2023-01-26 00:40:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-import configparser
+import os
 
 import openai
 import discord
 from discord.ext import commands
 
-config = configparser.ConfigParser()
-config.read("credentials.ini")
-
-# Set up the OpenAI API
-openai.api_key = config["Credentials"]["openai_api_key"]
-discord_bot_key = config["Credentials"]["discord_bot_key"]
+openai.api_key = os.environ["OPEN_AI_KEY"]
+discord_bot_key = os.environ["DISCORD_BOT_KEY"]
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -19,21 +15,25 @@ bot = commands.Bot(command_prefix='>', intents=intents)
 
 @bot.event
 async def on_message(message):
-    if message.author == bot.user:
-        return
+  if message.author == bot.user:
+    return
 
-    if message.content.startswith('!gpt'):
-        # Use the OpenAI API to generate a response to the message
-        response = openai.Completion.create(engine="text-davinci-002",
-                                            prompt=message.content[5:],
-                                            max_tokens=1024,
-                                            n=1,
-                                            stop=["[STOP]"])
+  if message.content.startswith('!gpt'):
+    # Use the OpenAI API to generate a response to the message
+    response = openai.Completion.create(engine="text-davinci-002",
+                                        prompt=message.content[5:],
+                                        max_tokens=1024,
+                                        n=1,
+                                        stop=["[STOP]"])
 
     try:
-        await message.channel.send(response["choices"][0]["text"])
+      await message.channel.send(response["choices"][0]["text"])
     except discord.errors.HTTPException:
-        await message.channel.send(response["choices"][0]["text"][:1999])
+      await message.channel.send(response["choices"][0]["text"][:1999])
 
 
-bot.run(discord_bot_key)
+if __name__ == "__main__":
+  try:
+    bot.run(discord_bot_key)
+  except:
+    os.system("kill 1")
