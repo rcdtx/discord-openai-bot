dbeb29ba38513a55533d951c3ebda38a432a129f
rcdhtx <rcdhtx@gmail.com>
2024-05-01 23:33:01 -0400

    updates

--- main.py
+++ main.py
@@ -1,46 +1,35 @@
 import os
 
 from openai import OpenAI
+from nextcord import Interaction, SlashOption
+from nextcord.ext import commands
 
-import discord
-from discord.ext import commands
+bot = commands.Bot()
 
 discord_bot_key = os.environ["DISCORD_BOT_KEY"]
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(command_prefix=">", intents=intents)
-
 client = OpenAI(api_key=os.environ["OPEN_AI_KEY"])
 
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
+@bot.slash_command(description="Ping command")
+async def ping(interaction: Interaction):
+    await interaction.response.send_message("Pong!")
 
-    if message.content.startswith("!gpt"):
-        # Use the OpenAI API to generate a response to the message
-        response = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "user", "content": message.content[5:]},
-            ],
-            max_tokens=200,
-            n=1,
-            stop=["[STOP]"],
-        )
 
-        try:
-            # await message.channel.send(response.choices[0].text)
-            await message.channel.send(response.choices[0].message.content)
-        except discord.errors.HTTPException:
-            await message.channel.send(response.choices[0])
+@bot.slash_command(description="Repeats your message")
+async def gpt(interaction: Interaction, arg: str = SlashOption(description="Prompt")):
+    # Use the OpenAI API to generate a response to the message
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": arg},
+        ],
+        max_tokens=200,
+        n=1,
+        stop=["[STOP]"],
+    )
+
+    await interaction.response.send_message(response.choices[0].message.content)
 
 
 if __name__ == "__main__":
-    try:
-        bot.run(discord_bot_key)
-    except:
-        os.system("kill 1")
+    bot.run(discord_bot_key)
